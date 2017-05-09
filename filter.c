a80ec591aa1ee7d41effebfb627571bf55f66ca8
marcospgp <marcospereiradev@gmail.com>
2017-05-09 22:58:16 +0100

    More work on filter.c

--- filter.c
+++ filter.c
@@ -1,11 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <string.h>
 
-#include "globals.h"
-#include "readLine.h"
+#include "includes/globals.h"
+#include "includes/readLine.h"
 
-int main (int argc, char** argv) {
+int main(int argc, char** argv) {
+
+	printf("%d\n", 1);
 
 	// Chamada com o formato "filter colunaA operador colunaB", exemplo: "filter 2 > 4"
 	// Operadores possíveis: =, >=, <=, >, <, !=
@@ -13,8 +16,10 @@ int main (int argc, char** argv) {
 	char* end;
 
 	long columnA = strtol(argv[1], &end, 10);
-	char operator = argv[2];
+	char operator = argv[2][0];
 	long columnB = strtol(argv[3], &end, 10);
+
+	printf("%d\n", 2);
 
 	// strtol error handling (strtol retorna 0 quando há um erro)
 	if ( (columnA == 0 || columnB == 0) && errno != 0 ) {
@@ -28,33 +33,35 @@ int main (int argc, char** argv) {
 		return EXIT_FAILURE;
 	}
 
+	printf("%d\n", 3);
+
 	// Ler linhas do stdin
 
-	char* buffer[PIPE_BUF];
+	char buffer[PIPE_BUF];
 	char* columnAValue = (char*) malloc(PIPE_BUF);
 	char* columnBValue = (char*) malloc(PIPE_BUF);
-	long columnAValueLong;
-	long columnBValueLong;
-	char* end2;
 
-	int i;
+	ssize_t i;
 
-	while ( (i = readLine(0, buffer, PIPE_BUF)) > 0) {
+	while ( (i = readLine(0, buffer, (long) PIPE_BUF)) > 0) {
 
 		// Obter valores das colunas
-		columnAValue = getElementValue(buffer, columnA, columnAValue, PIPE_BUF);
-		columnBValue = getElementValue(buffer, columnB, columnBValue, PIPE_BUF);
-
-		if (columnAValue < 1 || columnBValue < 1) {
-
+		if (
+			getElementValue(buffer, columnA, columnAValue, (long) PIPE_BUF) < 1 ||
+			getElementValue(buffer, columnB, columnBValue, (long) PIPE_BUF) < 1
+		) {
 			fprintf(stderr, "(filter) Error obtaining column values (getElementValue returned 0)");
 			return EXIT_FAILURE;
 		}
 
+		long columnAValueLong;
+		long columnBValueLong;
+		char* end2;
+
 		columnAValueLong = strtol(columnAValue, &end2, 10);
 		columnBValueLong = strtol(columnBValue, &end2, 10);
 
-		if ( (columnAValueLong == 0 || columnBValueLong) && errno != 0 ) {
+		if ( (columnAValueLong == 0 || columnBValueLong == 0) && errno != 0 ) {
 
 			fprintf(stderr, "(filter) Error converting column values to long (strtol returned 0 and set errno)");
 			return EXIT_FAILURE;
@@ -62,11 +69,12 @@ int main (int argc, char** argv) {
 
 		// Determinar operador a usar
 
+		printf("%ld %ld\n", columnAValueLong, columnBValueLong);
 	}
 
 	if (i < 0) {
 
-		fprintf(stderr, "(filter) Error reading input: %s (read() returned %d)\n", strerror(errno), i);
+		fprintf(stderr, "(filter) Error reading input: %s (read() returned %ld)\n", strerror(errno), i);
 		return EXIT_FAILURE;
 
 	} else {
