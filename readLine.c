72c75f56ba5a2646bc3d1a43dfd783d0bbcba6a9
marcospgp <marcospereiradev@gmail.com>
2017-05-09 13:41:04 +0100

    Add readLine.c

---
+++ readLine.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "readLine.h"
+
+/**
+ * Lê uma linha até ao tamanho máximo maxBytes
+ */
+ssize_t readln (int fd, char* buffer, size_t maxBytes) {
+
+	long bytesread = 0;
+	ssize_t i;
+	char c;
+
+	do {
+		i = read(fd, &c, 1);
+
+		if (i > 0) {
+			buffer[bytesread] = c;
+			bytesread++;
+		} else if (i < 0) {
+			buffer[bytesRead] = '\0';
+			return i;
+		}
+	} while ( i > 0 && bytesread < maxBytes && c != '\n');
+
+	buffer[bytesread] = '\0';
+
+	// Devolver o numero de bytes lidos
+	return bytesread;
+}
